9f82bbf315386463b01bb4350d3405a35938da9f
Nicholas Yoannou <nicholas@nicholas.dev>
2026-07-12 20:55:19 +0100

    chore: scaffold hayami-sdk package (tsup + vitest + strict TS, no DOM lib)

---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts',
+    types: 'src/types.ts',
+    'reddit/index': 'src/providers/reddit/index.ts',
+    'anilist/index': 'src/providers/anilist/index.ts',
+    'mal/index': 'src/providers/mal/index.ts',
+    'youtube/index': 'src/providers/youtube/index.ts',
+    'forum/index': 'src/providers/forum/index.ts',
+  },
+  format: ['esm'],
+  dts: true,
+  clean: true,
+  treeshake: true,
+  splitting: true,
+})
